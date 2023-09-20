5631b13230f4b83f1fa369ebec7a1967fb6edd1f
Justin Michel <justinmichel2000@gmail.com>
2023-09-20 11:24:11 +0200

    test

---
+++ assets/js/script.js
@@ -0,0 +1,128 @@
+let collection = [
+    {
+      picture:"" ,
+      name: "Gon Freecss",
+      typeNen: "Enhancement",
+      specialTech: ["Rock-paper-scissors", "Superhuman eyesight"],
+      affiliation: "Hunter Association",
+      description: "Gon Freecss is a Rookie Hunter and the son of Ging Freecss.Finding his father is Gon's motivation in becoming a Hunter ",
+      quote: "If you want to get to know someone, find out what makes them angry",
+    },
+
+    {
+      picture:"" ,
+      name: "Killua Zoldyck",
+      typeNen: "Transmutation",
+      specialTech: ["Lightning Palm", "Godspeed", "Thunderbolt"],
+      affiliation: ["Hunter Association", "Zoldyck's family"],
+      description: "Killua Zoldyck is the third child of Silva and Kikyo Zoldyck and the heir of the Zoldyck Family, until he runs away from home and becomes a Rookie Hunter. He is the best friend of Gon Freecss and is currently traveling with Alluka Zoldyck.",
+      quote: "I'm so tired of killing... I just want to be a kid. Hanging out, doing stuff with Gon. That's it",
+    },
+
+    {
+      picture:"" ,
+      name: "Kurapika",
+      typeNen: ["Conjuration", "Specialization"],
+      specialTech: ["Steal Chain", "Chain Jail", "Judgment Chain", "Emperor time"],
+      affiliation: ["Hunter Association", "Nostrade Family", "Zodiacs"],
+      description:"Kurapika is the last survivor of the Kurta Clan. He is a Blacklist Hunter and current leader of the organization founded by Light Nostrade. He is a member of the Zodiacs with the codename Rat. His goal is to avenge his clan and recover the remaining Scarlet Eyes." ,
+      quote: "I do not fear death. What I fear is that my rage will one day fade away",
+    },
+
+    {
+      picture:"" ,
+      name: "Chrollo Lucilfer",
+      typeNen: "Specialization",
+      specialTech: ["Skill Hunter", "Double Face"],
+      affiliation: "Phantom Troupe",
+      description:"Chrollo Lucilfer is the founder, leader, and member #0 of the Phantom Troupe.His physical strength ranked seventh among the group." ,
+      quote: "Making the ability yours while exploring the darkness within the soul of the original owner... that's the true pleasure of a skill hunter",
+    },
+
+    {
+      picture:"" ,
+      name: "Hisoka Morow",
+      typeNen: "Transmutation",
+      specialTech: ["Bungee Gum", "Texture Surprise"],
+      affiliation: "Hunter Association",
+      description: "Hisoka Morow is a Hunter and former member #4 of the Phantom Troupe; his physical strength ranked third in the group. He is always in searchfor strong opponents, and would spare those who have great potential, such as Gon and Killua in order for them to get strong enough to actually challenge him. ",
+      quote: "My greatest pleasure comes when such people crumple to their knees and I look down upon their disbelieving faces as their plans fail",
+    },
+
+    {
+      picture:"" ,
+      name: "Pulp Fiction",
+      typeNen: "Quentin Tarantino",
+      specialTech: 1994,
+      affiliation: ["Crime", "Drama"],
+      description: [
+        "John Travolta",
+        "Samuel L Jackson",
+        "Uma Thurman",
+        "Amanda Plummer",
+      ],
+      quote: "",
+    },
+
+    {
+      picture:"" ,
+      name: "Pulp Fiction",
+      typeNen: "Quentin Tarantino",
+      specialTech: 1994,
+      affiliation: ["Crime", "Drama"],
+      description: [
+        "John Travolta",
+        "Samuel L Jackson",
+        "Uma Thurman",
+        "Amanda Plummer",
+      ],
+      quote: "",
+    },
+
+    {
+      picture:"" ,
+      name: "Pulp Fiction",
+      typeNen: "Quentin Tarantino",
+      specialTech: 1994,
+      affiliation: ["Crime", "Drama"],
+      description: [
+        "John Travolta",
+        "Samuel L Jackson",
+        "Uma Thurman",
+        "Amanda Plummer",
+      ],
+      quote: "",
+    },
+
+    {
+      picture:"" ,
+      name: "Pulp Fiction",
+      typeNen: "Quentin Tarantino",
+      specialTech: 1994,
+      affiliation: ["Crime", "Drama"],
+      description: [
+        "John Travolta",
+        "Samuel L Jackson",
+        "Uma Thurman",
+        "Amanda Plummer",
+      ],
+      quote: "",
+    },
+
+    {
+      picture:"" ,
+      name: "Pulp Fiction",
+      typeNen: "Quentin Tarantino",
+      specialTech: 1994,
+      affiliation: ["Crime", "Drama"],
+      description: [
+        "John Travolta",
+        "Samuel L Jackson",
+        "Uma Thurman",
+        "Amanda Plummer",
+      ],
+      quote: "",
+    },
+  
+
+  ];
