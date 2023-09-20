c7088991af2f4653f37d640867ff29238d042a92
Justin Michel <justinmichel2000@gmail.com>
2023-09-20 12:30:11 +0200

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,103 +1,107 @@
 let collection = [
-    {
-      picture:"" ,
-      name: "Gon Freecss",
-      typeNen: "Enhancement",
-      specialTech: ["Rock-paper-scissors", "Superhuman eyesight"],
-      affiliation: "Hunter Association",
-      description: "Gon Freecss is a Rookie Hunter and the son of Ging Freecss.Finding his father is Gon's motivation in becoming a Hunter ",
-      quote: "If you want to get to know someone, find out what makes them angry",
-    },
+  {
+    picture:"" ,
+    name: "Gon Freecss",
+    typeNen: "Enhancement",
+    specialTech: ["Rock-paper-scissors", "Superhuman eyesight"],
+    affiliation: "Hunter Association",
+    description: "Gon Freecss is a Rookie Hunter and the son of Ging Freecss.Finding his father is Gon's motivation in becoming a Hunter ",
+    quote: "If you want to get to know someone, find out what makes them angry",
+  },
 
-    {
-      picture:"" ,
-      name: "Killua Zoldyck",
-      typeNen: "Transmutation",
-      specialTech: ["Lightning Palm", "Godspeed", "Thunderbolt"],
-      affiliation: ["Hunter Association", "Zoldyck's family"],
-      description: "Killua Zoldyck is the third child of Silva and Kikyo Zoldyck and the heir of the Zoldyck Family, until he runs away from home and becomes a Rookie Hunter. He is the best friend of Gon Freecss and is currently traveling with Alluka Zoldyck.",
-      quote: "I'm so tired of killing... I just want to be a kid. Hanging out, doing stuff with Gon. That's it",
-    },
+  {
+    picture:"" ,
+    name: "Killua Zoldyck",
+    typeNen: "Transmutation",
+    specialTech: ["Lightning Palm", "Godspeed", "Thunderbolt"],
+    affiliation: ["Hunter Association", "Zoldyck's family"],
+    description: "Killua Zoldyck is the third child of Silva and Kikyo Zoldyck and the heir of the Zoldyck Family, until he runs away from home and becomes a Rookie Hunter. He is the best friend of Gon Freecss and is currently traveling with Alluka Zoldyck.",
+    quote: "I'm so tired of killing... I just want to be a kid. Hanging out, doing stuff with Gon. That's it",
+  },
 
-    {
-      picture:"" ,
-      name: "Kurapika",
-      typeNen: ["Conjuration", "Specialization"],
-      specialTech: ["Steal Chain", "Chain Jail", "Judgment Chain", "Emperor time"],
-      affiliation: ["Hunter Association", "Nostrade Family", "Zodiacs"],
-      description:"Kurapika is the last survivor of the Kurta Clan. He is a Blacklist Hunter and current leader of the organization founded by Light Nostrade. He is a member of the Zodiacs with the codename Rat. His goal is to avenge his clan and recover the remaining Scarlet Eyes." ,
-      quote: "I do not fear death. What I fear is that my rage will one day fade away",
-    },
+  {
+    picture:"" ,
+    name: "Kurapika",
+    typeNen: ["Conjuration", "Specialization"],
+    specialTech: ["Steal Chain", "Chain Jail", "Judgment Chain", "Emperor time"],
+    affiliation: ["Hunter Association", "Nostrade Family", "Zodiacs"],
+    description:"Kurapika is the last survivor of the Kurta Clan. He is a Blacklist Hunter and current leader of the organization founded by Light Nostrade. He is a member of the Zodiacs with the codename Rat. His goal is to avenge his clan and recover the remaining Scarlet Eyes." ,
+    quote: "I do not fear death. What I fear is that my rage will one day fade away",
+  },
 
-    {
-      picture:"" ,
-      name: "Chrollo Lucilfer",
-      typeNen: "Specialization",
-      specialTech: ["Skill Hunter", "Double Face"],
-      affiliation: "Phantom Troupe",
-      description:"Chrollo Lucilfer is the founder, leader, and member #0 of the Phantom Troupe.His physical strength ranked seventh among the group." ,
-      quote: "Making the ability yours while exploring the darkness within the soul of the original owner... that's the true pleasure of a skill hunter",
-    },
+  {
+    picture:"" ,
+    name: "Chrollo Lucilfer",
+    typeNen: "Specialization",
+    specialTech: ["Skill Hunter", "Double Face"],
+    affiliation: "Phantom Troupe",
+    description:"Chrollo Lucilfer is the founder, leader, and member #0 of the Phantom Troupe.His physical strength ranked seventh among the group." ,
+    quote: "Making the ability yours while exploring the darkness within the soul of the original owner... that's the true pleasure of a skill hunter",
+  },
 
-    {
-      picture:"" ,
-      name: "Hisoka Morow",
-      typeNen: "Transmutation",
-      specialTech: ["Bungee Gum", "Texture Surprise"],
-      affiliation: "Hunter Association",
-      description: "Hisoka Morow is a Hunter and former member #4 of the Phantom Troupe; his physical strength ranked third in the group. He is always in searchfor strong opponents, and would spare those who have great potential, such as Gon and Killua in order for them to get strong enough to actually challenge him. ",
-      quote: "My greatest pleasure comes when such people crumple to their knees and I look down upon their disbelieving faces as their plans fail",
-    },
+  {
+    picture:"" ,
+    name: "Hisoka Morow",
+    typeNen: "Transmutation",
+    specialTech: ["Bungee Gum", "Texture Surprise"],
+    affiliation: "Hunter Association",
+    description: "Hisoka Morow is a Hunter and former member #4 of the Phantom Troupe; his physical strength ranked third in the group. He is always in searchfor strong opponents, and would spare those who have great potential, such as Gon and Killua in order for them to get strong enough to actually challenge him. ",
+    quote: "My greatest pleasure comes when such people crumple to their knees and I look down upon their disbelieving faces as their plans fail",
+  },
 
-    {
-      picture:"" ,
-      name: "Ging Freecss",
-      typeNen: "Unknown",
-      specialTech: ["Remote Punch", "Ultrasound", "Phasing Bullets"],
-      affiliation: ["Hunter Association", "Continent Expedition Team"],
-      description: "Ging Freecss is the father of Gon Freecss. He is a Double-Star Ruins Hunter (though he can apply for a Triple-Star License), and a former Zodiac with the codename Boar.",
-      quote: "I'm enjoying the journey. So if your destination is the same as mine, enjoy the side trips. A lot. Something more important than the thing you're hunting could be right there by the side of the road",
-    },
+  {
+    picture:"" ,
+    name: "Ging Freecss",
+    typeNen: "Unknown",
+    specialTech: ["Remote Punch", "Ultrasound", "Phasing Bullets"],
+    affiliation: ["Hunter Association", "Continent Expedition Team"],
+    description: "Ging Freecss is the father of Gon Freecss. He is a Double-Star Ruins Hunter (though he can apply for a Triple-Star License), and a former Zodiac with the codename Boar.",
+    quote: "I'm enjoying the journey. So if your destination is the same as mine, enjoy the side trips. A lot. Something more important than the thing you're hunting could be right there by the side of the road",
+  },
 
-    {
-      picture:"" ,
-      name: "Kite",
-      typeNen: "Conjuration",
-      specialTech: "Crazy Slots",
-      affiliation: "Amateur Hunters",
-      description:"Kite was a Hunter and Ging Freecss' student. After his death, he was reborn as a Chimera Ant. Colt named them Reina, after his late sister, but they took to calling themself Kite once again." ,
-      quote: "If you lose even a little of this resolve and start backsliding, I'll kill you. You can live in peace knowing that",
-    },
+  {
+    picture:"" ,
+    name: "Kite",
+    typeNen: "Conjuration",
+    specialTech: "Crazy Slots",
+    affiliation: "Amateur Hunters",
+    description:"Kite was a Hunter and Ging Freecss' student. After his death, he was reborn as a Chimera Ant. Colt named them Reina, after his late sister, but they took to calling themself Kite once again." ,
+    quote: "If you lose even a little of this resolve and start backsliding, I'll kill you. You can live in peace knowing that",
+  },
 
-    {
-      picture:"" ,
-      name: "Meruem",
-      typeNen: "Emission",
-      specialTech: ["Aura Synthesis", "Rage Blast", "Metamorphosis"],
-      affiliation: "Chimera Ants",
-      description: "Meruem was the most powerful offspring of the Chimera Ant Queen. He was known as the King of the Chimera Ants, and served as the main antagonist of the Chimera Ant arc.",
-      quote: "In the hands of an incompetent, power brings nothing but ruin",
-    },
+  {
+    picture:"" ,
+    name: "Meruem",
+    typeNen: "Emission",
+    specialTech: ["Aura Synthesis", "Rage Blast", "Metamorphosis"],
+    affiliation: "Chimera Ants",
+    description: "Meruem was the most powerful offspring of the Chimera Ant Queen. He was known as the King of the Chimera Ants, and served as the main antagonist of the Chimera Ant arc.",
+    quote: "In the hands of an incompetent, power brings nothing but ruin",
+  },
 
-    {
-      picture:"" ,
-      name: "Neferpitou",
-      typeNen: "Specialization",
-      specialTech: ["Terpsichora", "Doctor Blythe", "Puppeteering"],
-      affiliation: "Royal Guards",
-      description: "Neferpitou, nicknamed Pitou , was a cat-humanoid Chimera Ant and the firstborn of the Chimera Ant King's three Royal Guards.",
-      quote: "If anyone had to bear the brunt of his rage, I'm glad I was the one to die!",
-    },
+  {
+    picture:"" ,
+    name: "Neferpitou",
+    typeNen: "Specialization",
+    specialTech: ["Terpsichora", "Doctor Blythe", "Puppeteering"],
+    affiliation: "Royal Guards",
+    description: "Neferpitou, nicknamed Pitou , was a cat-humanoid Chimera Ant and the firstborn of the Chimera Ant King's three Royal Guards.",
+    quote: "If anyone had to bear the brunt of his rage, I'm glad I was the one to die!",
+  },
 
-    {
-      picture:"" ,
-      name: "Isaac Netero",
-      typeNen: "Enhancement",
-      specialTech: ["100-Type Guanyin", "Bodhisattva"],
-      affiliation: "Hunter Association",
-      description: "Isaac Netero was the 12th Chairman of the Hunter Association and the Head of the Exam Commission.In his youth, he was extolled as the most powerful Nen user in the world, and retained dreadful strength even in his old age.",
-      quote: "You believe I cannot pray with a single arm? A prayer comes from the heart",
-    },
-  
+  {
+    picture:"" ,
+    name: "Isaac Netero",
+    typeNen: "Enhancement",
+    specialTech: ["100-Type Guanyin", "Bodhisattva"],
+    affiliation: "Hunter Association",
+    description: "Isaac Netero was the 12th Chairman of the Hunter Association and the Head of the Exam Commission.In his youth, he was extolled as the most powerful Nen user in the world, and retained dreadful strength even in his old age.",
+    quote: "You believe I cannot pray with a single arm? A prayer comes from the heart",
+  },
 
-  ];
+
+];
+
+
+
+
